dbbda881f942ab501db21e94919e56395b9d2fb2
shubhsk88 <shubham88singh@gmail.com>
2021-06-08 23:15:02 +0530

    Fixing the class-transformer issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Validator } from 'class-validator';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
