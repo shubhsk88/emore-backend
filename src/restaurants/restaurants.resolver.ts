619fce249bdbe4a33a427fb1e045094f19d06a78
shubhsk88 <shubham88singh@gmail.com>
2021-06-08 23:14:15 +0530

    Adding the pipe

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,15 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant-dto';
 
 @Resolver()
 export class RestaurantResolver {
   @Query((returns) => Boolean)
   isBig() {
+    return true;
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto) {
     return true;
   }
 }
