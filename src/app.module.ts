78000cd2660c62703ceeb3b2ca17ff306277d032
shubhsk88 <shubham88singh@gmail.com>
2021-06-09 07:59:17 +0530

    Adding the env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
+
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV == 'development'
+          ? '.env.development'
+          : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
