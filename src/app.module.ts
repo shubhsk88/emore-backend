17d4580bb339fe7db3a2e40244ddf8f20893b242
shubhsk88 <shubham88singh@gmail.com>
2021-06-09 22:58:44 +0530

    Setting up the typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant';
 
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
@@ -31,7 +32,8 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+      entities: [Restaurant],
       logging: true,
     }),
     GraphQLModule.forRoot({
---
+++ src/restaurants/entities/restaurant.ts
@@ -0,0 +1,21 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field((type) => String)
+  @Column()
+  name: string;
+
+  @Field((type) => Boolean)
+  @Column()
+  isVegan: boolean;
+
+  @Field((type) => String)
+  @Column()
+  restaurantOwner: string;
+}
