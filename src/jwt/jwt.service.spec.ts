13deaba732ef8eac210630889281dd2b92717bf6
Shubham Singh <shubham88singh@gmail.com>
2021-08-06 10:29:33 +0530

    Defining the service

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,23 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from 'src/common/common.constant';
+import { JwtService } from './jwt.service';
+
+describe('JwtService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: 'aha' },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
