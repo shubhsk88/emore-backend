88a12add07f6a8bfce19eb18e5d6b43fd90e68ba
Shubham Singh <shubham88singh@gmail.com>
2021-08-25 22:31:59 +0530

    FIxing the module issue

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
-import { RestaurantResolver } from './restaurants.resolver';
+import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantResolver, RestaurantService],
+  providers: [RestaurantResolver, RestaurantService, CategoryResolver],
   imports: [TypeOrmModule.forFeature([Restaurant, Category])],
 })
 export class RestaurantsModule {}
